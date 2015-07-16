790867440991d804b6cae7b96ab3e567c4dfe93f
Sunjay Varma <varma.sunjay@gmail.com>
2015-07-16 02:18:53 -0400

    Refactored some names into constants and made a way cooler function

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-const WIDTH = 600;
-const HEIGHT = 400;
-const f = (x, y) => 2*(Math.sin(0.5*x) + Math.sin(0.5*y));
+const exp = Math.exp, sin = Math.sin, cos = Math.cos;
 
-const app = new Application(WIDTH, HEIGHT);
+const WINDOW_WIDTH = 600;
+const WINDOW_HEIGHT = 400;
 
-const planeWidth = 50;
-const planeHeight = 50;
-const resolution = 4;
-const geometry = new ProceduralGeometry(planeWidth, planeHeight, planeWidth * resolution, planeHeight * resolution);
+const TERRAIN_WIDTH = 50;
+const TERRAIN_LENGTH = 50;
+const TERRAIN_HEIGHT = 10;
+const TERRAIN_RESOLUTION = 4;
+const f = (x, y) => TERRAIN_HEIGHT*(cos(x/(TERRAIN_WIDTH/4))*cos(x/5) + cos(y/(TERRAIN_LENGTH/4))*cos(y/5));
+
+const app = new Application(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+const geometry = new ProceduralGeometry(TERRAIN_WIDTH, TERRAIN_LENGTH, TERRAIN_WIDTH * TERRAIN_RESOLUTION, TERRAIN_LENGTH * TERRAIN_RESOLUTION);
 geometry.apply(f);
 
 const material = new THREE.MeshLambertMaterial({color: 0xFFBF00, side: THREE.DoubleSide});
@@ -19,8 +23,8 @@ app.camera.lookAt(plane.position);
 app.addToScene(plane);
 
 const axisHelper = new THREE.AxisHelper(5);
-axisHelper.position.x = planeWidth/2;
-axisHelper.position.y = planeHeight/2;
+axisHelper.position.x = TERRAIN_WIDTH/2;
+axisHelper.position.y = TERRAIN_LENGTH/2;
 app.addToScene(axisHelper);
 
 const container = document.getElementById('world-creator');
